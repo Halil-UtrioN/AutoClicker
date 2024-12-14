bc1f0bea86a98205147bf851191320a42a1e3cb4
Halil İbrahim ÇAKIR <hibrahim54@cakirhalil.com>
2024-12-14 23:43:20 +0300

    bug fix.

--- src/app.py
+++ src/app.py
@@ -3,16 +3,37 @@
 import pyautogui
 import time
 from datetime import datetime  # Zaman bilgisini almak için
+import logging # Hata mesajlarını kaydetmek için
+
+# Logging yapılandırması
+logging.basicConfig(
+    filename='AutoClick.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+logging.info("Uygulama Başlatıldı")
 
 second = 1
-second = int(input("Ne kadar sürede bir(varsayılan 1): "))
+while True:
+    try:
+        second = int(input("Saniye cinsinden tam sayı giriniz (varsayılan 1): "))
+        logging.info(f"Değer verildi. Değer = {second}")
+        break #Doğru şekilde değer verilirse
+    except ValueError:
+        logging.warning("Varsayılan değer atandı")
+        print("Varsayılan değer atandı.")
+        break # Değer verilemdi, varsayılan değer 1
 
 try:
+    logging.info("Döngü başlatıldı")
     print("Program başlıyor. Çıkmak için Ctrl+C'ye basın.")
     while True:
         pyautogui.click()  # Fare tıklaması yapar
         current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Şu anki zamanı alır
         print(f"{current_time} Tıklandı!")
+        logging.info("Tıklama gerçekleşti")
         time.sleep(second)  # Belirtilen sürede bekler
 except KeyboardInterrupt:
+    logging.info("Program sonlandırıldı")
     print("Program durduruldu.")
